a45899af0c6df9124ab5edd2b62959607f20bb88
sushantpardhi <sushantpardhi186@gmail.com>
2024-11-10 22:41:35 +0530

    Add about and contact routes to the application

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
